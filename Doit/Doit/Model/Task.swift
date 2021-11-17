ae2457a2e1a2f6ee210eac28fb3f6ccf2cdb9b83
Vitalii Kryvoshapka <v.kryvoshapka@icloud.com>
2021-11-18 00:17:42 +0200

    Create Model & Temp Tasks

---
+++ Doit/Doit/Model/Task.swift
@@ -0,0 +1,38 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Vitalii Kryvoshapka on 17.11.2021.
+//
+
+import Foundation
+
+//Task type
+enum TaskPriority{
+    case normal
+    case important
+}
+
+//Task condition
+enum TaskStatus{
+    case planned
+    case completed
+}
+
+
+//Protocol to type "Task"
+protocol TaskProtocol{
+    //Name
+    var title: String { get set }
+    var type: TaskPriority {get set}
+    var status: TaskStatus {get set}
+}
+
+// Essence "TASK"
+struct Task: TaskProtocol{
+    var title: String
+    
+    var type: TaskPriority
+    
+    var status: TaskStatus
+}
---
+++ Doit/Doit/Model/TaskStorage.swift
@@ -0,0 +1,35 @@
+//
+//  TaskStorage.swift
+//  Doit
+//
+//  Created by Vitalii Kryvoshapka on 18.11.2021.
+//
+
+import Foundation
+
+//Protocol to essence "TaskStorage"
+protocol TaskStorageProtocol{
+    func loadTasks() -> [TaskProtocol]
+    func saveTasks(_ tasks: [TaskProtocol])
+}
+
+
+// "TaskStorage" ESSENCE
+class TaskStorage: TaskStorageProtocol{
+    func loadTasks() -> [TaskProtocol] {
+        //Temporarily, return test task collection
+        let testTasks: [TaskProtocol] = [Task(title: "Clean Macbook", type: .normal, status: .planned),
+                                         Task(title: "Watch Dexter", type: .important, status: .planned),
+                                         Task(title: "Learn Swift", type: .important, status: .completed),
+                                         Task(title: "By new iPhone", type: .normal, status: .completed),
+                                         Task(title: "Buy flowers to Alisa", type: .important, status: .planned),
+                                         Task(title: "Call grandmother", type: .important, status: .planned)
+        ]
+        return testTasks
+    }
+    
+    func saveTasks(_ tasks: [TaskProtocol]) {
+        <#code#>
+    }
+    
+}
